afea5e37247592afe82783632a57a33ccde1db64
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-05-29 16:40:55 -0600

    Create test.slurm.sh

---
+++ test.slurm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account=communitybem
+#SBATCH --time=00:01:00
+#SBATCH --ntasks=3
+#SBATCH --job-name=mpirun
+#SBATCH --output=mpirun%j.out
+#SBATCH --error=mpirun%j.err
+# Set the directory where you want to install the software, Change to the source code directory
+DIR="/home/lwu4/mpich_slurm_experiments"
+cd $DIR
+pwd
+make
+echo "Running on $(hostname)"
+# Execute the program
+mpirun ./mpiapp.exe
+make clean
