bed3204a3a0eee5decf7e3b4f9290476f42b4ff2
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-05-29 11:31:43 -0600

    Update mpiapp.c

--- mpiapp.c
+++ mpiapp.c
@@ -6,14 +6,14 @@ void spawn_children(void);
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
-    int rank, size;
+    int rank, world_size, name_len;
     char processor_name[MPI_MAX_PROCESSOR_NAME];
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Get_processor_name(processor_name, &size);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Get_processor_name(processor_name, &name_len);
      
-    printf("Rank %d from MPI_COMM_WORLD %d (size %d ) at processor %s\n", rank, MPI_COMM_WORLD, size, processor_name);
+    printf("Rank %d from MPI_COMM_WORLD %d (size %d ) at processor %s\n", rank, MPI_COMM_WORLD, world_size, processor_name);
     MPI_Finalize();
     return 0;
 }
