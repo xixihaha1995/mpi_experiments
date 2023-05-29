e6c8de52a59e14c8392e6b2ce072a97457dc07e8
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-05-29 11:29:15 -0600

    init

---
+++ mpiapp.c
@@ -0,0 +1,20 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MPI_MAX_PROCESSOR_NAME 128
+void spawn_children(void);
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+
+    MPI_Info_create(&info);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Get_processor_name(processor_name, &size);
+     
+    printf("Rank %d from MPI_COMM_WORLD %d (size %d ) at processor %s\n", rank, MPI_COMM_WORLD, size, processor_name);
+    MPI_Finalize();
+    return 0;
+}
