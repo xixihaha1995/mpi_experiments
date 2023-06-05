6b27344c8ec54217988cf073dfdfa66548492ead
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-06-05 12:22:08 -0600

    Update parent.c

--- c_pbs_parent_child/parent.c
+++ c_pbs_parent_child/parent.c
@@ -21,11 +21,15 @@ void spawn_children(void) {
     MPI_Status status;
     MPI_Comm child_comm;
 
+    MPI_Info info;
+    MPI_Info_create(&info);
+    MPI_Info_set(info, "MPI_UNIVERSE_SIZE", nbr_child); 
+
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Get_processor_name(processor_name, &name_len);
     
-    MPI_Comm_spawn(command, MPI_ARGV_NULL, nbr_child, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &child_comm, MPI_ERRCODES_IGNORE);
+    MPI_Comm_spawn(command, MPI_ARGV_NULL, nbr_child, info, 0, MPI_COMM_WORLD, &child_comm, MPI_ERRCODES_IGNORE);
 
     printf("Parent: Rank %d from comm %d (size %d) at processor %s\n", rank, MPI_COMM_WORLD,world_size, processor_name);
 }
